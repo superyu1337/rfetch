cc8eab3012fe6db795c31d01b9690d34928ee061
Janek <development@superyu.xyz>
2021-10-28 00:47:03 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn write(color: Option<Color>, input: &str) -> Result<()> {
 }
 
 fn write_column(input1: &str, input2: &str) {
-    write(Some(Color::Cyan), input1).unwrap();
+    write(Some(Color::Rgb(77, 195, 255)), input1).unwrap();
     write(Some(Color::White), input2).unwrap();
 }
 
@@ -30,15 +30,15 @@ fn main() {
     let sys = System::new_with_specifics(specifics);
     let cpu0 = &sys.processors()[0];
     let uptime_string = get_uptime_str(sys.uptime());
-    let mem_string = format!("{} MB / {} MB", sys.used_memory() / 1000, sys.total_memory() / 1000);
+    let mem_string = format!("{} MB / {} MB", sys.used_memory() / 1024, sys.total_memory() / 1024);
 
-    write(Some(Color::Magenta), &whoami::username()).unwrap();
+    write(Some(Color::Rgb(255, 128, 251)), &whoami::username()).unwrap();
     write(Some(Color::White), "@").unwrap();
-    write(Some(Color::Magenta), &sys.host_name().unwrap()).unwrap();
+    write(Some(Color::Rgb(255, 128, 251)), &sys.host_name().unwrap()).unwrap();
 
     write_column("\nos        ", &sys.name().unwrap());
+    write_column("\nkernel    ", &sys.kernel_version().unwrap());
     write_column("\nuptime    ", &uptime_string);
     write_column("\ncpu       ", &cpu0.brand());
-    write_column("\nkernel    ", &sys.kernel_version().unwrap());
     write_column("\nmemory    ", &mem_string);
 }
